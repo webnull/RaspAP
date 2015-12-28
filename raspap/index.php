aa630c383d4d650676885d89d0d62d933e070a15
Damian Kęska <webnull.www@gmail.com>
2015-12-28 11:20:34 +0100

    Added additional security

--- raspap/index.php
+++ raspap/index.php
@@ -1,9 +1,18 @@
 <?php
+$parsed = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+
 if (in_array(pathinfo(parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH), PATHINFO_EXTENSION), [
     'jpeg', 'jpg', 'gif', 'css', 'js', 'png', 'ico', 'woff', 'ttf'
 ]))
 {
     return false;
+}
+elseif (in_array(strtolower(pathinfo($parsed, PATHINFO_BASENAME)), [
+    '.gitignore', 'composer.json', 'LICENSE', 'README.md',
+]))
+{
+    header('HTTP/1.1 403 Forbidden');
+    exit;
 }
 
 require './vendor/pantheraframework/panthera/lib/init.php';
