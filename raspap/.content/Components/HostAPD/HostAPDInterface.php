81ccf41a1eb7535776b7c0ab14bf0b31411ec77c
Damian Kęska <webnull.www@gmail.com>
2016-01-03 20:11:02 +0100

    Changed config permissions #2

--- raspap/.content/Components/HostAPD/HostAPDInterface.php
+++ raspap/.content/Components/HostAPD/HostAPDInterface.php
@@ -640,7 +640,7 @@ class HostAPDInterface extends AbstractConfigClass
         }
 
         $result = IniSupport::write_ini_file($this->data, $this->configPath, false, '');
-        chmod($this->configPath, 774);
+        chmod($this->configPath, 775);
         return $result;
     }
 }
