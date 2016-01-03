9e2f96c5b6301e1b977205a628426f5fde99665c
Damian Kęska <webnull.www@gmail.com>
2016-01-03 17:32:19 +0100

    Fixed PAM support to work with different versions

--- raspapd/raspapd-pam.py
+++ raspapd/raspapd-pam.py
@@ -10,7 +10,9 @@ if len(lines) < 2:
     print('Expected only two lines: user, password')
     sys.exit(1)
 
-if pam.authenticate(lines[0].replace('\n', '').strip(), lines[1].replace('\n', '').strip()):
+auth = pam.authenticate if hasattr(pam, 'authenticate') else pam.pam().authenticate
+
+if auth(lines[0].replace('\n', '').strip(), lines[1].replace('\n', '').strip()):
     print('Success')
     sys.exit(0)
 
