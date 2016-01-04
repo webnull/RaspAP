8e77f848328f4a03483b40a45057e04091e68688
Damian Kęska <webnull.www@gmail.com>
2016-01-04 12:44:17 +0100

    Added database deploy, removed db from repo

--- install.sh
+++ install.sh
@@ -73,6 +73,11 @@ fi
 cd ../
 cp ./ /usr/share/webapps/raspap -pr
 
+# deploy database
+cd /usr/share/webapps/raspap/raspap
+sudo -u raspap ./vendor/pantheraframework/panthera/lib/Binaries/deploy Build/Database/Migrate
+sudo -u raspap ./vendor/pantheraframework/panthera/lib/Binaries/deploy Build/Routing/Cache
+
 echo "RaspAP installed in /usr/share/webapps/raspap"
 echo "To allow you'r user to overwrite RaspAP files type: "
 echo "gpasswd -a your-login-here raspap"
